88fca0dfcfc6d2443e74346656b288bfdb7b72c1
ailobe <aitorlopezbeltran@gmail.com>
2019-08-26 11:17:39 +0200

    Remove unused decorator

--- app/decorators/decidim/organization_decorator.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# Commenting this to avoid the following error while generating the test_app:
-# TypeError: superclass mismatch for class Organization
-
-# ::Decidim::Organization.class_eval do
-#   has_many :redirect_rules, foreign_key: :decidim_organization_id, class_name: "RedirectRule"
-# end
